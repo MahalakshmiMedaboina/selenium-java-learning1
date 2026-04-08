acf75680746cad51dd203e20b70bed03cc61ba0d
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-04-09 00:20:34 +0530

    Topic covered: Iterators in HashSet

--- src/collections/HashSetExample.java
+++ src/collections/HashSetExample.java
@@ -1,6 +1,9 @@
 package collections;
 
 import java.util.HashSet;
+import java.util.Iterator;
+
+import com.google.common.collect.Iterators;
 
 public class HashSetExample {
 
@@ -14,13 +17,21 @@ public class HashSetExample {
 		hs.add("USA");
 		hs.add("UK");
 		hs.add("INDIA");
-		hs.add("UK");
+		hs.add("UK"); // this will be ignored, as HashSet will not accept duplicate values.
 		
 		System.out.println(hs);
 		System.out.println(hs.isEmpty());
 		System.out.println(hs.size());
 		
-
+		//iterators
+		Iterator<String> i = hs.iterator();
+//		i.next(); //prints a value, if we have 4 values to print then 4times we need to write "next" it increments each time n prints all, 
+		
+		// To avoid writing many times next, will follow below one
+		while(i.hasNext()) { //here hasNext will check it has a value next or not, if not loop will end.
+			
+			System.out.println(i.next());
+		}
 	}
 
 }
